00397999ca8a4d8afed46a17a517a92a975afa5f
Luciano Mancini <pianolucho@hotmail.com>
2023-04-01 20:11:35 -0300

    modificaciones varias

--- script.js
+++ script.js
@@ -10,6 +10,37 @@ var swiper = new swiper(".mySwiper-1", {
         nexEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     }
-}
+})
 
-)
+var swiper = new Swiper(".mySwiper-2", {
+    slidesPerView:3,
+    spaceBetween: 20,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+        nexEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    breakpoints : {
+    0: {
+        slidesPerView:1,
+        },
+        520: {
+            slidesPerView:2,
+        },
+        950: {
+            slidesPerView:3,
+        }
+}
+})
+
+let tabInputs = document.querySelectorAll(".tabInput");
+
+tabInputs.forEach(function(input) {
+    input.addEventListener('change', function() {
+let id = input.ariaValueMax;
+let thisSwiper = document.getElemntById('swiper' + id);
+thisSwiper.swiper.update()
+    })
+})
+
