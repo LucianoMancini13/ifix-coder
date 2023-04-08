824929202bbe7bea3bf440dcb4e5dd5c72f4c40f
Luciano Mancini <pianolucho@hotmail.com>
2023-04-08 02:01:44 -0300

    responsive completo

--- script.js
+++ script.js
@@ -1,47 +1,12 @@
-var swiper = new swiper(".mySwiper-1", {
-    slidesPerView:1,
-    spaceBetween: 30,
-    loop: true,
-    pagination{
-        el: ".swiper-pagination",
-        clickable: true,
-    }
-    navigation:{
-        nexEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    }
+
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
 })
 
-var swiper = new Swiper(".mySwiper-2", {
-    slidesPerView:3,
-    spaceBetween: 20,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    navigation: {
-        nexEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev"
-    },
-    breakpoints : {
-    0: {
-        slidesPerView:1,
-        },
-        520: {
-            slidesPerView:2,
-        },
-        950: {
-            slidesPerView:3,
-        }
-}
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
 })
-
-let tabInputs = document.querySelectorAll(".tabInput");
-
-tabInputs.forEach(function(input) {
-    input.addEventListener('change', function() {
-let id = input.ariaValueMax;
-let thisSwiper = document.getElemntById('swiper' + id);
-thisSwiper.swiper.update()
-    })
-})
-
-
